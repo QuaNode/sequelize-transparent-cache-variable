45254bdf34cb21c24d99643973bafd31691ef640
Ahmed <ahmed.ragab@me.com>
2017-09-23 22:34:19 +0200

    add lru-cache

--- VariableAdaptor.js
+++ VariableAdaptor.js
@@ -1,31 +1,33 @@
+var LRU = require('lru-cache');
+
 class VariableAdaptor {
-  constructor (store = {}) {
-    this.store = store
+  constructor (options) {
+    this.store = LRU(options);
   }
 
   _ensureModel (model) {
-    if (!this.store[model]) {
-      this.store[model] = {}
+    if (!this.store.get(model)) {
+      this.store.set(model, {})
     }
   }
 
   set ([model, id], value) {
     this._ensureModel(model)
 
-    this.store[model][id] = value
+    this.store.get(model)[id] = value
     return Promise.resolve()
   }
 
   get ([model, id]) {
     this._ensureModel(model)
 
-    return Promise.resolve(this.store[model][id])
+    return Promise.resolve(this.store.get(model)[id])
   }
 
   del ([model, id]) {
     this._ensureModel(model)
 
-    delete this.store[model][id]
+    delete this.store.get(model)[id]
     Promise.resolve()
   }
 }
